7f05be3ed22c8a2ed736ce80ce5c2500536085c0
Kuppuswamy Sathyanarayanan <sathyaosid@gmail.com>
2019-01-07 00:49:55 -0800

    kdev: Update kdev
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyaosid@gmail.com>

--- kdev/_kdev.py
+++ kdev/_kdev.py
@@ -31,27 +31,27 @@ from shutil import copy2
 
 valid_str = lambda x: True if x is not None and isinstance(x, basestring) and len(x) > 0 else False
 
-def get_recipe_name(recipedir, logger=None):
+def get_recipe_name(recipe_dir, logger=None):
     recipe = JSONParser(pkg_resources.resource_filename('kdev', 'schemas/board-schema.json'),
-                             os.path.join(recipedir, 'board.json'), extend_defaults=True,
+                             os.path.join(recipe_dir, 'board.json'), extend_defaults=True,
                              os_env=True, logger=logger)
     recipecfg = recipe.get_cfg()
 
     return recipecfg["recipe-name"] if valid_str(recipecfg["recipe-name"]) else None
 
 class KdevBuild(object):
-    def __init__(self, ksrcdir, rsrcdir, recipedir, outdir, logger=None):
+    def __init__(self, kernel_dir, rootfs_dir, recipe_dir, out_dir, logger=None):
         self.logger = logger or logging.getLogger(__name__)
 
-        self.ksrc = os.path.abspath(ksrcdir)
+        self.ksrc = os.path.abspath(kernel_dir)
         self.kparams = None
         self.kobj = None
 
-        self.rsrc = os.path.abspath(rsrcdir)
+        self.rsrc = os.path.abspath(rootfs_dir)
         self.rparams = None
         self.robj = None
 
-        self.recipedir = os.path.abspath(recipedir)
+        self.recipe_dir = os.path.abspath(recipe_dir)
         self.recipename = None
         self.bparams = None
 
@@ -62,27 +62,28 @@ class KdevBuild(object):
         if not is_valid_kernel(self.ksrc, logger):
             return
 
-        if not os.path.exists(self.recipedir):
-            self.logger.error("Recipe dir %s does not exist", self.recipedir)
+        if not os.path.exists(self.recipe_dir):
+            self.logger.error("Recipe dir %s does not exist", self.recipe_dir)
             raise AttributeError
 
         for file in ['board.json', 'kernel.config']:
-            if not os.path.exists(os.path.join(self.recipedir, file)):
+            if not os.path.exists(os.path.join(self.recipe_dir, file)):
                 self.logger.error("Missing %s file in recipe folder" % file)
                 raise AttributeError
 
-        self.recipename = get_recipe_name(recipedir)
+        self.recipename = get_recipe_name(recipe_dir)
 
         if self.recipename is None:
             self.logger.error("Invalid recipe name %s" % self.recipename)
             raise AttributeError
 
-        self.kout = os.path.join(os.path.abspath(outdir), self.recipename, 'kernel')
-        self.rout = os.path.join(os.path.abspath(outdir), self.recipename, 'rootfs')
-        self.iout = os.path.join(os.path.abspath(outdir), self.recipename, 'images')
+        self.kout = os.path.join(os.path.abspath(out_dir), self.recipename, 'obj/kernel')
+        self.idir = os.path.join(os.path.abspath(out_dir), self.recipename, 'install/rootfs')
+        self.rout = os.path.join(os.path.abspath(out_dir), self.recipename, 'obj/rootfs')
+        self.iout = os.path.join(os.path.abspath(out_dir), self.recipename, 'images')
 
         self.recipe = JSONParser(pkg_resources.resource_filename('kdev', 'schemas/board-schema.json'),
-                                 os.path.join(recipedir, 'board.json'), extend_defaults=True,
+                                 os.path.join(recipe_dir, 'board.json'), extend_defaults=True,
                                  os_env=True, logger=logger)
         self.recipecfg = self.recipe.get_cfg()
 
@@ -93,18 +94,47 @@ class KdevBuild(object):
 
         self.kparams = self.recipecfg["kernel-params"]
         self.rparams =  self.recipecfg["rootfs-params"]
+        self.iparams = self.recipecfg["initramfs-params"]
         self.bparams = self.recipecfg["bootimg-params"]
-        self.oparams = self.recipecfg["out-image"]
 
-        self.robj = RootFS(self.rparams["rootfs-name"], self.rsrc, self.rout, self.logger)
+        self.robj = RootFS(self.rparams["name"], self.rsrc, self.idir, self.rout, self.logger)
+        self.iobj = RootFS(self.iparams["name"], self.rsrc, self.idir, self.rout, self.logger)
 
-        self.kobj = BuildKernel(src_dir=self.ksrc, out_dir=self.kout, arch=self.kparams["arch_name"],
-                                cc=self.kparams["compiler_options"]["CC"],
-                                cflags=self.kparams["compiler_options"]["cflags"],
+        self.kobj = BuildKernel(src_dir=self.ksrc, out_dir=self.kout, arch=self.kparams["arch-name"],
+                                cc=self.kparams["compiler-options"]["CC"],
+                                cflags=self.kparams["compiler-options"]["cflags"],
                                 logger=self.logger)
 
+    def initramfs_build(self):
+        if not self.iparams["enable-build"]:
+            self.logger.warning("Initramfs build option is not enabled")
+            return False
+
+        if self.iobj is None:
+            self.logger.error("Invalid initramfs object")
+            return False
+
+        get_val = lambda x: self.rparams[x] if len(self.iparams[x]) > 0 else None
+
+        if get_val("config-file") is not None:
+            config =  os.path.join(self.recipe_dir, get_val("config-file"))
+        else:
+            config = None
+
+        if get_val("diffconfig-file") is not None:
+            diffconfig =  os.path.join(self.recipe_dir, get_val("diffconfig-file"))
+        else:
+            diffconfig = None
+
+        self.iobj.build(self.iparams["source-url"], self.iparams["source-branch"],
+                        config, diffconfig, self.iparams["arch-name"],
+                        self.iparams["compiler-options"]["CC"],
+                        self.iparams["compiler-options"]["cflags"],
+                        )
+        return True
+
     def kernel_build(self):
-        if not self.kparams["build"]:
+        if not self.kparams["enable-build"]:
             self.logger.warning("Kernel build option is not enabled")
             return False
 
@@ -112,7 +142,7 @@ class KdevBuild(object):
             self.logger.error("Invalid kernel build object")
             return False
 
-        self.kobj.copy_newconfig(os.path.join(self.recipedir, self.kparams["config-file"]))
+        self.kobj.copy_newconfig(os.path.join(self.recipe_dir, self.kparams["config-file"]))
 
         cobj = KernelConfig(src=self.kobj.cfg, logger=self.logger)
         config_list = []
@@ -132,7 +162,7 @@ class KdevBuild(object):
             self.logger.error(err)
             self.logger.error(out)
 
-        ret, out, err = self.kobj.make_modules_install(flags=["INSTALL_MOD_PATH=%s" % self.rout])
+        ret, out, err = self.kobj.make_modules_install(flags=["INSTALL_MOD_PATH=%s" % self.idir])
 
         status = True if ret == 0 else False
 
@@ -143,7 +173,7 @@ class KdevBuild(object):
         return status
 
     def rootfs_build(self):
-        if not self.rparams["build"]:
+        if not self.rparams["enable-build"]:
             self.logger.warning("Rootfs build option is not enabled")
             return False
 
@@ -151,8 +181,23 @@ class KdevBuild(object):
             self.logger.error("Invalid rootfs object")
             return False
 
-        self.robj.build(config=os.path.join(self.recipedir, self.rparams["rootfs-config"]),
-                        branch=self.rparams["rootfs-branch"])
+        get_val = lambda x: self.rparams[x] if len(self.rparams[x]) > 0 else None
+
+        if get_val("config-file") is not None:
+            config =  os.path.join(self.recipe_dir, get_val("config-file"))
+        else:
+            config = None
+
+        if get_val("diffconfig-file") is not None:
+            diffconfig =  os.path.join(self.recipe_dir, get_val("diffconfig-file"))
+        else:
+            diffconfig = None
+
+        self.robj.build(self.rparams["source-url"], self.rparams["source-branch"],
+                        config, diffconfig, self.rparams["arch-name"],
+                        self.rparams["compiler-options"]["CC"],
+                        self.rparams["compiler-options"]["cflags"],
+                        )
 
         return True
 
@@ -180,8 +225,8 @@ class KdevBuild(object):
 
         self.robj.set_hostname(self.rparams["hostname"])
 
-        if os.path.exists(os.path.join(self.recipedir, 'rootfs-updates')):
-            self.robj.update_rootfs(os.path.join(self.recipedir, 'rootfs-updates'), self.rout)
+        if os.path.exists(os.path.join(self.recipe_dir, 'rootfs-updates')):
+            self.robj.update_rootfs(os.path.join(self.recipe_dir, 'rootfs-updates'), self.rout)
 
         return True
 
@@ -197,7 +242,7 @@ class KdevBuild(object):
         self.robj.gen_image(self.oparams["rimage-type"], os.path.join(self.iout, self.oparams["rimage-name"]))
         self.robj.gen_image("cpio", os.path.join(self.iout, self.oparams["initramfs-name"]))
 
-        copy2(os.path.join(self.kout, 'arch', self.kparams["arch_name"], 'boot/bzImage'),
+        copy2(os.path.join(self.kout, 'arch', self.kparams["arch-name"], 'boot/bzImage'),
               os.path.join(self.iout, self.oparams["kimage-name"]))
 
         return True
